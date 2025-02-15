14b00c140f545ab9dcedd56ac6abd0076231a323
Zephan Wong <zephanwong@hotmail.com>
2025-02-15 17:42:18 +0800

    fsfs

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Scene } from "./Components/Scene";
+
+export default function Home() {
+  return (
+    <main className="relative">
+     <Scene />
+     <section id= "hero"
+      className = "h-screen flex items-end jusitfy-center relative"
+      >
+        <div id="text-1" className="text-cente mb-64 z-10">
+          <h1 className="text-8xl font-medium mb-5">
+            Welcome to your mama's womb
+            </h1> 
+            <p className="text-4xl font-light text-gray-500">
+              Your mum gay
+            </p>
+        </div>
+      
+     </section>
+    </main>
+  );
+}
